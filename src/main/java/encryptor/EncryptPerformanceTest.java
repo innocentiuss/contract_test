76aa5b2f7efc68b93b582a19b246c89ea829d86d
szncu <549528220@qq.com>
2024-03-05 09:27:35 +0800

    git init

--- src/main/java/encryptor/EncryptPerformanceTest.java
+++ src/main/java/encryptor/EncryptPerformanceTest.java
@@ -5,62 +5,67 @@ import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Security;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 public class EncryptPerformanceTest {
 
-    private static final int GENERATING_COUNT_TARGET = 10;
+//    private static int GENERATING_COUNT_TARGET = 10;
+    private static List<Integer> GENERATING_COUNT_TARGETS = Arrays.asList(10, 50, 100, 200, 500, 1000, 2000, 5000, 10000);
 
     public static void main(String[] args) throws Exception {
         Security.addProvider(new BouncyCastleProvider());
         EncryptService encryptService = new EncryptService(3072, 256);
-        // obj invoke ---------------- ecc
-        long start = System.currentTimeMillis();
-        for (int attempts = 0; attempts < GENERATING_COUNT_TARGET; attempts++) {
-            encryptService.generateECCKeyPair();
-        }
-        long end = System.currentTimeMillis();
+        for (Integer generatingCountTarget : GENERATING_COUNT_TARGETS) {
+            // obj invoke ---------------- ecc
+            long start = System.currentTimeMillis();
+            for (int attempts = 0; attempts < generatingCountTarget; attempts++) {
+                encryptService.generateECCKeyPair();
+            }
+            long end = System.currentTimeMillis();
 
-        log.info("generating {} ecc pair from service at {} milliseconds", GENERATING_COUNT_TARGET, end - start);
+            log.info("generating {} ecc pair from service at {} milliseconds", generatingCountTarget, end - start);
 
-        // static invoke
-        start = System.currentTimeMillis();
-        for (int attempts = 0; attempts < GENERATING_COUNT_TARGET; attempts++) {
-            EncryptUtils.generateECCKeyPair();
-        }
-        end = System.currentTimeMillis();
-        log.info("generating {} ecc pair from static method at {} milliseconds", GENERATING_COUNT_TARGET, end - start);
+            // static invoke
+            start = System.currentTimeMillis();
+            for (int attempts = 0; attempts < generatingCountTarget; attempts++) {
+                EncryptUtils.generateECCKeyPair();
+            }
+            end = System.currentTimeMillis();
+            log.info("generating {} ecc pair from static method at {} milliseconds", generatingCountTarget, end - start);
 
-        // obj invoke ------------ rsa
-        start = System.currentTimeMillis();
-        for (int attempts = 0; attempts < GENERATING_COUNT_TARGET; attempts++) {
-            encryptService.generateRSAKeyPair();
-        }
-        end = System.currentTimeMillis();
-        log.info("generating {} ecc pair from static method at {} milliseconds", GENERATING_COUNT_TARGET, end - start);
+            // obj invoke ------------ rsa
+            start = System.currentTimeMillis();
+            for (int attempts = 0; attempts < generatingCountTarget; attempts++) {
+                encryptService.generateRSAKeyPair();
+            }
+            end = System.currentTimeMillis();
+            log.info("generating {} ecc pair from static method at {} milliseconds", generatingCountTarget, end - start);
 
-        // static invoke
-        start = System.currentTimeMillis();
-        for (int attempts = 0; attempts < GENERATING_COUNT_TARGET; attempts++) {
-            EncryptUtils.generateRSAKeyPair();
-        }
-        end = System.currentTimeMillis();
-        log.info("generating {} ecc pair from static method at {} milliseconds", GENERATING_COUNT_TARGET, end - start);
+            // static invoke
+            start = System.currentTimeMillis();
+            for (int attempts = 0; attempts < generatingCountTarget; attempts++) {
+                EncryptUtils.generateRSAKeyPair();
+            }
+            end = System.currentTimeMillis();
+            log.info("generating {} ecc pair from static method at {} milliseconds", generatingCountTarget, end - start);
 
-        // obj invoke -------------- ecdsa
-        start = System.currentTimeMillis();
-        for (int attempts = 0; attempts < GENERATING_COUNT_TARGET; attempts++) {
-            encryptService.generateECDSAPair();
-        }
-        end = System.currentTimeMillis();
-        log.info("generating {} ecc pair from static method at {} milliseconds", GENERATING_COUNT_TARGET, end - start);
+            // obj invoke -------------- ecdsa
+            start = System.currentTimeMillis();
+            for (int attempts = 0; attempts < generatingCountTarget; attempts++) {
+                encryptService.generateECDSAPair();
+            }
+            end = System.currentTimeMillis();
+            log.info("generating {} ecc pair from static method at {} milliseconds", generatingCountTarget, end - start);
 
-        // static invoke
-        start = System.currentTimeMillis();
-        for (int attempts = 0; attempts < GENERATING_COUNT_TARGET; attempts++) {
-            EncryptUtils.generateECDSAPair();
+            // static invoke
+            start = System.currentTimeMillis();
+            for (int attempts = 0; attempts < generatingCountTarget; attempts++) {
+                EncryptUtils.generateECDSAPair();
+            }
+            end = System.currentTimeMillis();
+            log.info("generating {} ecc pair from static method at {} milliseconds", generatingCountTarget, end - start);
         }
-        end = System.currentTimeMillis();
-        log.info("generating {} ecc pair from static method at {} milliseconds", GENERATING_COUNT_TARGET, end - start);
     }
 }
